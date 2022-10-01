97329a439045f7cde036465e54f95f072ea8c959
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-10-01 14:59:35 +0600

    new data post api implemented

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ app.use(express.json());
 fs.readFile("data.json", "utf-8", (err, data) => {
   const info = JSON.parse(data);
 
+  //get random data
   app.get("/user/random", (req, res) => {
     let x = Math.floor(Math.random() * 10 + 1);
     const data = info[x];
     res.send(data);
   });
 
+  //get all data
   app.get("/user/all", (req, res) => {
     res.send(data);
+  });
+
+  //post new data
+  app.post("/user/save", (req, res) => {
+    const data = req.body;
+    info.push(data);
+    res.send(info);
   });
 });
 
